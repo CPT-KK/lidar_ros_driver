5d574afbd1f78a7af0e8ccdb1dc2db75ffbbd28f
cptkkk <cptkk@qq.com>
2023-10-16 15:50:05 +0800

    update at lake

--- src/lidar_usv_det.cpp
+++ src/lidar_usv_det.cpp
@@ -61,18 +61,34 @@
 using namespace std;
 
 ros::Publisher postPCPub, objectPub;
-ros::Subscriber imu_sub;
-double m_usv_length = 0.0;
-double m_usv_width = 0.0;
-int m_usv_intensity = 0;
+ros::Subscriber imuSub;
+float X_MIN = 0.0f;
+float X_MAX = 0.0f;
+float Y_MIN = 0.0f;
+float Y_MAX = 0.0f;
+float Z_MIN = 0.0f;
+float Z_MAX = 0.0f;
+float USV_LENGTH = 0.0f;
+float USV_WIDTH = 0.0f;
+float USV_INTENSITY_MIN = 0.0f;
+int OUTLIER_STATIC_CHECK_POINT = 0;
+float OUTLIER_STATIC_TOL = 0;
+int CLUSTER_SIZE_MIN = 0;
+int CLUSTER_SIZE_MAX = 0;
+float CLUSTER_TOL = 0;
+float TARGET_VESSEL_LENGTH_MIN = 0.0f;
+float TARGET_VESSEL_LENGTH_MAX = 0.0f;
+float TARGET_VESSEL_WIDTH_MIN = 0.0f;
+float TARGET_VESSEL_WIDTH_MAX = 0.0f;
+
 int counter = 0;
 
 // 包含功能1：remove the usv body from the lidar pointscloud
-pcl::PointCloud<pcl::PointXYZI>::Ptr lidar1PC;
-pcl::PointCloud<pcl::PointXYZI>::Ptr lidar2PC;
-pcl::PointCloud<pcl::PointXYZI>::Ptr lidar3PC;
-pcl::PointCloud<pcl::PointXYZI>::Ptr lidarRawPC;
-pcl::PointCloud<pcl::PointXYZI>::Ptr pc;
+pcl::PointCloud<pcl::PointXYZI>::Ptr lidar1PC(new pcl::PointCloud<pcl::PointXYZI>);
+pcl::PointCloud<pcl::PointXYZI>::Ptr lidar2PC(new pcl::PointCloud<pcl::PointXYZI>);
+pcl::PointCloud<pcl::PointXYZI>::Ptr lidar3PC(new pcl::PointCloud<pcl::PointXYZI>);
+pcl::PointCloud<pcl::PointXYZI>::Ptr lidarRawPC(new pcl::PointCloud<pcl::PointXYZI>);
+pcl::PointCloud<pcl::PointXYZI>::Ptr pc(new pcl::PointCloud<pcl::PointXYZI>);
 std::vector<pcl::PointIndices> clusterIndices;
 
 #ifdef USE_IMU
@@ -125,9 +141,9 @@ inline void clusterExt(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud) {
     kdTree->setInputCloud(cloud);
 
     pcl::EuclideanClusterExtraction<pcl::PointXYZI> ecExtraction;
-    ecExtraction.setClusterTolerance(2.0f);      // 点之间相隔大于10m的不被认为是一个聚类
-    ecExtraction.setMinClusterSize(40);       // 一个聚类里所允许的最少点数
-    ecExtraction.setMaxClusterSize(5000);    // 一个聚类里所允许的最多点数
+    ecExtraction.setClusterTolerance(CLUSTER_TOL);      // 点之间相隔大于10m的不被认为是一个聚类
+    ecExtraction.setMinClusterSize(CLUSTER_SIZE_MIN);       // 一个聚类里所允许的最少点数
+    ecExtraction.setMaxClusterSize(CLUSTER_SIZE_MAX);    // 一个聚类里所允许的最多点数
     ecExtraction.setSearchMethod(kdTree);     // 设置搜索方法为 KdTreee
     ecExtraction.setInputCloud(cloud);     // 设置被搜索的点云
 
@@ -195,7 +211,7 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
     pcl::fromROSMsg(*lidar1, *lidar2PC);
     pcl::fromROSMsg(*lidar2, *lidar3PC);
     *lidarRawPC = *lidar1PC + *lidar2PC + *lidar3PC;
-
+    ROS_INFO("Raw pointcloud: %d", static_cast<int>(lidarRawPC->size())); 
     ROS_INFO("Processed from msgs: %e s", (ros::Time::now() - t0).toSec()); 
     t0 = ros::Time::now();
 
@@ -208,25 +224,25 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
         }
 
         // Outer box filter
-        if (lidarRawPC->points[i].x < -200.0f || lidarRawPC->points[i].x > 200.0f) {
+        if (lidarRawPC->points[i].x < X_MIN || lidarRawPC->points[i].x > X_MAX) {
             continue;
         }
-        if (lidarRawPC->points[i].y < -70.0f || lidarRawPC->points[i].y > 70.0f) {
+        if (lidarRawPC->points[i].y < Y_MIN || lidarRawPC->points[i].y > Y_MAX) {
             continue;
         }
-        if (lidarRawPC->points[i].z < -1.5f || lidarRawPC->points[i].z > 5.0f) {
+        if (lidarRawPC->points[i].z < Z_MIN || lidarRawPC->points[i].z > Z_MAX) {
             continue;
         }
 
         // USV filter
-        if (lidarRawPC->points[i].x < 3.6f && lidarRawPC->points[i].x > -3.6f && lidarRawPC->points[i].y < 1.25f && lidarRawPC->points[i].x > -1.25f) {
+        if (lidarRawPC->points[i].x < USV_LENGTH && lidarRawPC->points[i].x > -USV_LENGTH && lidarRawPC->points[i].y < USV_WIDTH && lidarRawPC->points[i].y > -USV_WIDTH) {
             continue;
         }
 
         // 现在的点应该是 OK 的
         lidarRawPC->points[i].z = 0.0f;
         Eigen::Vector3d thisPoint(lidarRawPC->points[i].x, lidarRawPC->points[i].y, lidarRawPC->points[i].z);
-        thisPoint = imu_pose * thisPoint;
+        // thisPoint = imu_pose * thisPoint;
 
         pcl::PointXYZI point_pcl = lidarRawPC->points[i];
         point_pcl.x = thisPoint[0];
@@ -234,24 +250,25 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
         point_pcl.z = thisPoint[2];
         pc->push_back(point_pcl);
     }
-
+    ROS_INFO("Boxed pointcloud: %d", static_cast<int>(pc->size()));
     ROS_INFO("Box filter costs: %e s", (ros::Time::now() - t0).toSec());
     t0 = ros::Time::now();
 
     // Outlier filters
     cloudFilter(pc, 0.1f);
+    ROS_INFO("Outlier removed pointcloud: %d", static_cast<int>(pc->size()));
     ROS_INFO("Removing outliers costs: %e s", (ros::Time::now() - t0).toSec());
     t0 = ros::Time::now();
 
     // Cluster extraction
     clusterExt(pc);
-    ROS_INFO("Extract clusters: %e s", (ros::Time::now() - t0).toSec());
+    ROS_INFO("Extract %d clusters, cost %e s", static_cast<int>(clusterIndices.size()), (ros::Time::now() - t0).toSec());
     t0 = ros::Time::now();
 
     // Cluster state estimation
     geometry_msgs::PoseArray objects;
-    objects.header.stamp = lidar0->header.stamp;
-    objects.header.frame_id = "USV_FLU";
+    objects.header.stamp = ros::Time::now();
+    objects.header.frame_id = "base_link";
     objects.poses.reserve(clusterIndices.size());
 #pragma omp parallel for num_threads(8)    
     for (std::vector<pcl::PointIndices>::const_iterator it = clusterIndices.begin(); it != clusterIndices.end(); ++it) {
@@ -273,7 +290,7 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
         float cenY = 0.0f;
         clusterDirection(cloudCluster, objVector, len, wid, lwRatio, cenX, cenY);
 
-        if (len > 30.0f || wid > 10.0f) {
+        if (len > TARGET_VESSEL_LENGTH_MAX || wid > TARGET_VESSEL_WIDTH_MAX) {
             continue;
         }
 
@@ -298,7 +315,7 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
     sensor_msgs::PointCloud2 postPC;
     pcl::toROSMsg(*pc, postPC);
     postPC.header.stamp = lidar0->header.stamp;
-    postPC.header.frame_id = "USV_FLU";
+    postPC.header.frame_id = "base_link";
     postPCPub.publish(postPC);
     pc->clear();
     ROS_INFO("Processed pointcloud published in %e s\n", (ros::Time::now() - t0).toSec());
@@ -307,34 +324,56 @@ void lidarcallback(const sensor_msgs::PointCloud2::ConstPtr& lidar0, const senso
 int main(int argc, char** argv) {
     ros::init(argc, argv, "lidar_usv_det");
     ros::NodeHandle nh("~");
+    ros::Rate rate(10.0);
 
+    // 加载参数
+    nh.param<float>("x_min", X_MIN, -200.0f);
+    nh.param<float>("x_max", X_MAX, 200.0f);
+    nh.param<float>("y_min", Y_MIN, -70.0f);
+    nh.param<float>("y_max", Y_MAX, 70.0f);
+    nh.param<float>("z_min", Z_MIN, -2.0f);
+    nh.param<float>("z_max", Z_MAX, 4.0f);
+    nh.param<float>("usv_length", USV_LENGTH, 3.6f);
+    nh.param<float>("usv_width", USV_WIDTH, 1.25f);
+    nh.param<float>("usv_intensity", USV_INTENSITY_MIN, 20);
+    nh.param<int>("outlier_static_check_point", OUTLIER_STATIC_CHECK_POINT, 50);
+    nh.param<float>("outlier_static_tol", OUTLIER_STATIC_TOL, 1.5f);
+    nh.param<int>("cluster_size_min", CLUSTER_SIZE_MIN, 50);
+    nh.param<int>("cluster_size_max", CLUSTER_SIZE_MAX, 10000);
+    nh.param<float>("cluster_tol", CLUSTER_TOL, 20);
+    nh.param<float>("target_vessel_length_min", TARGET_VESSEL_LENGTH_MIN, 1.5f);
+    nh.param<float>("target_vessel_length_max", TARGET_VESSEL_LENGTH_MAX, 25.0f);
+    nh.param<float>("target_vessel_width_min", TARGET_VESSEL_WIDTH_MIN, 0.5f);
+    nh.param<float>("target_vessel_width_max", TARGET_VESSEL_WIDTH_MAX, 8.0f);
+
+    ROS_INFO("Params: Intensity: %.2f | X box filter: %.2f | Y box filter: %.2f", USV_INTENSITY_MIN, USV_LENGTH, USV_WIDTH);
+
+    // 定义发送
     postPCPub = nh.advertise<sensor_msgs::PointCloud2>("/filter/lidar", 1);
     objectPub = nh.advertise<geometry_msgs::PoseArray>("/filter/target", 1);
 
-    nh.param<double>("usv_length", m_usv_length, 3.0);
-    nh.param<double>("usv_width", m_usv_width, 1.2);
-    nh.param<int>("usv_intensity", m_usv_intensity, 20);
-    ROS_INFO("Params: Intensity: %d | X box filter: %.2f | Y box filter: %.2f", m_usv_intensity, m_usv_length, m_usv_width);
+    // 定义订阅
+    imuSub = nh.subscribe("/mavros/imu/data", 1, imuCallback);
 
-    imu_sub = nh.subscribe("/mavros/imu/data", 1, imuCallback);
+    // 定义时间同步订阅
+    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar0(nh, "/livox/lidar_192_168_147_231", 5);
+    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar1(nh, "/livox/lidar_192_168_147_232", 5);
+    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar2(nh, "/livox/lidar_192_168_147_233", 5);
 
-    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar0(nh, "/livox/lidar_192_168_147_231", 3);
-    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar1(nh, "/livox/lidar_192_168_147_232", 3);
-    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_lidar2(nh, "/livox/lidar_192_168_147_233", 3);
-
+    // 使用 ApproximateTime 策略定义同步器
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, sensor_msgs::PointCloud2, sensor_msgs::PointCloud2> MySyncPolicy;
     message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(3), sub_lidar0, sub_lidar1, sub_lidar2);
+    
+    // 注册回调函数，当消息时间接近并准备同步时，该函数会被调用
     sync.registerCallback(boost::bind(&lidarcallback, _1, _2, _3));
 
     // 预分配内存
-    lidar1PC->points.reserve(10000);
-    lidar2PC->points.reserve(10000);
-    lidar3PC->points.reserve(10000);
-    lidarRawPC->points.reserve(30000);
-    pc->points.reserve(30000);
+    lidar1PC->points.reserve(50000);
+    lidar2PC->points.reserve(50000);
+    lidar3PC->points.reserve(50000);
+    lidarRawPC->points.reserve(150000);
+    pc->points.reserve(150000);
     clusterIndices.reserve(20);
-
-    ros::Rate rate(10.0);
 
     while (ros::ok()) {
         ros::spinOnce();
